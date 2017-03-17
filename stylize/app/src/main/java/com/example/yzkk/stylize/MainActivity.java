71113094d947ea38b692d5217d24f1cbb60b0a5c
yizhikong <343802694@qq.com>
2017-03-17 19:16:54 +0800

    succeed to apply tensorflow model

--- stylize/app/src/main/java/com/example/yzkk/stylize/MainActivity.java
+++ stylize/app/src/main/java/com/example/yzkk/stylize/MainActivity.java
@@ -1,10 +1,33 @@
 package com.example.yzkk.stylize;
 
 import android.app.AlertDialog;
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.CursorLoader;
 import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.res.AssetManager;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.UUID;
 import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
 
@@ -13,16 +36,73 @@ public class MainActivity extends AppCompatActivity {
     private static final int INPUT_SIZE = 224;
     private static final int IMAGE_MEAN = 117;
     private static final float IMAGE_STD = 1;
-    private static final String MODEL_FILE = "file:///android_asset/stylize_quantized.pb";
+    private static final String MODEL_FILE = "file:///android_asset/wave.pb";
     private Stylizer stylizer;
     private Executor executor = Executors.newSingleThreadExecutor();
 
+    private  Button open_btn;
+    private  Button stylize_btn;
+    private  Button save_btn;
+
+    private ImageView img_view;
+    private Bitmap bitmap;
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         initModel();
-        tip();
+
+        open_btn = (Button)findViewById(R.id.open_btn);
+        stylize_btn = (Button)findViewById(R.id.stylize_btn);
+        save_btn = (Button)findViewById(R.id.save_btn);
+        img_view = (ImageView)findViewById(R.id.img_view);
+
+
+
+        open_btn.setOnClickListener(new Button.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setType("image/*");
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(intent, 1);
+            }
+        });
+
+        stylize_btn.setOnClickListener(new Button.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                try {
+                    Stylizer.stylizeImage(bitmap, stylizer);
+                    img_view.setImageBitmap(bitmap);
+                    save_btn.setEnabled(true);
+                } catch (Exception e) {
+                    Log.e("Exception", e.getMessage(), e);
+                }
+            }
+        });
+
+        save_btn.setOnClickListener(new Button.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                String dir = Environment.getExternalStorageDirectory().getAbsolutePath() + "/";
+                String fileName = "stylize" + UUID.randomUUID().toString();
+                try {
+                    // should be ok but fail, I don't know why
+                    File file = new File(dir + fileName + ".jpg");
+                    FileOutputStream out = new FileOutputStream(file);
+                    bitmap.compress(Bitmap.CompressFormat.JPEG, 100, out);
+                    out.flush();
+                    out.close();
+                    Uri uri = Uri.fromFile(file);
+                    sendBroadcast(new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE, uri));
+                    Toast.makeText(MainActivity.this, "saved", Toast.LENGTH_SHORT ).show();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
     }
 
     private void initModel() {
@@ -30,11 +110,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void run() {
                 try {
-                    stylizer = Stylizer.create(getAssets(),
-		                                       MODEL_FILE,
-			    		                       INPUT_SIZE,
-			                                   IMAGE_MEAN,
-					                           IMAGE_STD);
+                    stylizer = Stylizer.create(getAssets(), MODEL_FILE);
 	            } catch (final Exception e) {
 	                throw new RuntimeException("Error initializing TensorFlow", e);
 	            }
@@ -42,18 +118,22 @@ public class MainActivity extends AppCompatActivity {
 	    });
     }
 
-    private void tip() {
-        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
-        builder.setMessage("You have arrived here!");
-        builder.setTitle("Congratulation");
-        builder.setPositiveButton("Yeah", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                dialog.dismiss();
-                MainActivity.this.finish();
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode == RESULT_OK) {
+            Uri uri = data.getData();
+            Log.e("uri", uri.toString());
+            ContentResolver cr = this.getContentResolver();
+            try {
+                Bitmap bm = BitmapFactory.decodeStream(cr.openInputStream(uri));
+                bitmap = bm.copy(bm.getConfig(), true);
+                img_view.setImageBitmap(bm);
+                stylize_btn.setEnabled(true);
+            } catch (FileNotFoundException e) {
+                Log.e("Exception", e.getMessage(),e);
             }
-        });
-        builder.create().show();
+        }
+        super.onActivityResult(requestCode, resultCode, data);
     }
 
 }
--- stylize/app/src/main/java/com/example/yzkk/stylize/Stylizer.java
+++ stylize/app/src/main/java/com/example/yzkk/stylize/Stylizer.java
@@ -4,32 +4,57 @@ package com.example.yzkk.stylize;
  * Created by yzkk on 17-3-13.
  */
 import android.content.res.AssetManager;
+import android.graphics.Bitmap;
+
 import org.tensorflow.contrib.android.TensorFlowInferenceInterface;
 
 public class Stylizer {
 
     private TensorFlowInferenceInterface inferenceInterface;
-    private int inputSize;
-    private int imageMean;
-    private float imageStd;
+
+    private static final String INPUT_NODE = "img_placeholder";
+    private static final String OUTPUT_NODE = "add_37";
+
 
     public Stylizer() {
     }
 
     public static Stylizer create(AssetManager assetManager,
-                                  String modelFileName,
-				  int inputSize,
-				  int imageMean,
-				  float imageStd) {
+                                  String modelFileName) {
         Stylizer stylizer = new Stylizer();
         stylizer.inferenceInterface = new TensorFlowInferenceInterface();
         if (stylizer.inferenceInterface.initializeTensorFlow(assetManager, modelFileName) != 0) {
             throw new RuntimeException("TF initialization failed");
         }
-	stylizer.inputSize = inputSize;
-	stylizer.imageMean = imageMean;
-	stylizer.imageStd = imageStd;
-	return stylizer; 
+	return stylizer;
+    }
+
+    public static void stylizeImage(final Bitmap img, Stylizer stylizer) {
+        int[] intValues = new int[img.getWidth() * img.getHeight()];
+        float[] floatValues = new float[img.getWidth() * img.getHeight() * 3];
+        img.getPixels(intValues, 0, img.getWidth(), 0, 0, img.getWidth(), img.getHeight());
+
+        for (int i = 0; i < intValues.length; ++i) {
+            final int val = intValues[i];
+            floatValues[i * 3] = ((val >> 16) & 0xFF);
+            floatValues[i * 3 + 1] = ((val >> 8) & 0xFF);
+            floatValues[i * 3 + 2] = (val & 0xFF);
+        }
+
+        // Copy the input data into TensorFlow.
+        stylizer.inferenceInterface.fillNodeFloat(
+                INPUT_NODE, new int[] {1, img.getWidth(), img.getHeight(), 3}, floatValues);
+        stylizer.inferenceInterface.runInference(new String[] {OUTPUT_NODE});
+        stylizer.inferenceInterface.readNodeFloat(OUTPUT_NODE, floatValues);
+
+        for (int i = 0; i < intValues.length; ++i) {
+            intValues[i] = 0xFF000000
+                            | (((int) floatValues[i * 3]) << 16)
+                            | (((int) floatValues[i * 3 + 1]) << 8)
+                            | ((int) floatValues[i * 3 + 2]);
+        }
+
+        img.setPixels(intValues, 0, img.getWidth(), 0, 0, img.getWidth(), img.getHeight());
     }
 
 }
